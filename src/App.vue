a59ceea0c27e24ff1512da79173beb4c5d5c2c77
amor novilunio III <lifecycl308@gmail.com>
2023-07-26 12:13:27 +0800

    final

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="bg-gray-100">
+        <router-view/>
+    </main>
+</template>
+<!-- Global Style -->
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+.font-poppins{
+  font-family: 'Poppins', sans-serif;
+}
+
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
---
+++ src/components/input.vue
@@ -0,0 +1,19 @@
+<template>
+    <input class="shadow-lg p-3 outline-none rounded focus:shadow-inner focus:bg-gray-100 hover:shadow-inner transition-all"
+        v-bind="$attrs" Name={{name}} Type={{type}} tabindex={{tabIndex}} placeholder={{placeHolder}} :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)">
+</template>
+
+
+<script>
+export default {
+    name: "InputComponent",
+    props: {
+        placeHolder: String,
+        name: String,
+        type: String,
+        tabIndex: String,
+        modelValue: String
+    }
+}
+</script>
